c4f6905326c228e9f1cf77118cb2415179e07fa0
brascome <brascome@uab.edu>
2015-01-24 13:23:13 -0500

    added game scene with placehoders

---
+++ Assets/Scripts/GameDirector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameDirector : MonoBehaviour {
+
+    public static GameDirector instance = null;
+
+    public GameObject Neil = null;
+   
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+}
---
+++ Assets/Scripts/NealController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NealController : MonoBehaviour {
+
+    public GameObject Neal = null;
+    private string strHorizontalAxis = "Horizontal";
+    private string strVerticalAxis = "Vertical";
+    private string strFireButton;
+    private float fltRotate = 0;
+    private float fltMove = 0;
+    private float fltNealDirection = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        fltRotate = Input.GetAxis(strHorizontalAxis);
+        fltMove = Input.GetAxis(strVerticalAxis);
+
+        fltNealDirection += fltRotate * Time.deltaTime;
+        Neal.transform.rotation = Quaternion.EulerAngles(0.0f, fltNealDirection, 0.0f);
+        Neal.transform.position += Neal.transform.forward * fltMove * Time.deltaTime;
+	}
+}
