f3154ee8b687c82a69f4c30a596268e31caf9c84
brascome <brascome@uab.edu>
2015-01-24 18:43:24 -0500

    added attack beacon and damage beacon to alien ai

--- Assets/Plugins/Alien.cs
+++ Assets/Plugins/Alien.cs
@@ -6,20 +6,41 @@ public class Alien : MonoBehaviour {
     public enum enAlienBehaviors
     { 
         Attack,
-        RunAway
+        RunAway,
+        Wander
     }
 
-    public enAlienBehaviors Behavior = enAlienBehaviors.RunAway;
+    public enAlienBehaviors Behavior = enAlienBehaviors.Attack;
+
+    /// <summary>
+    /// running speed
+    /// </summary>
     public float fltSpeed = 1.0f;
-    public float fltBeaconMinAttackDistance = 1.0f;
+    /// <summary>
+    /// When the alien gets to this distance from the beacon, it attacks
+    /// </summary>
+    public float fltBeaconAttackDistance = 1.0f;
+    /// <summary>
+    /// this is the minimum distance an alien will run from a light when in RunAway mode
+    /// </summary>
     public float fltLightSafeDistance = 5.0f;
+    /// <summary>
+    /// The current health
+    /// </summary>
+    public float fltHealth = 100.0f;
+    /// <summary>
+    /// The alien will change from attack mode to RunAway mode when his health falls below this point
+    /// </summary>
+    public float fltHealthRunAwayThreshold = 50.0f;
+    /// <summary>
+    /// The rate the alien does damage to a beacon during an attack
+    /// </summary>
+    public float fltBeaconDamageRate = 0.01f;
 
     private Neil neil = null;
     private Ship ship = null;
-    
-    private float fltHealth = 100.0f;
-    private float fltSunburn = 0.0f;
-    
+
+    string strDisplay = "";
 
     private GameObject[] beacons = null;
     private GameObject closestBeacon = null;
@@ -28,26 +49,36 @@ public class Alien : MonoBehaviour {
 	/// 
 	/// </summary>
 	void Start () {
-        Behavior = enAlienBehaviors.RunAway;
         neil = GameDirector.instance.neil;
         ship = GameDirector.instance.ship;
+        StartWave();
 	}
 	
 	/// <summary>
 	/// 
 	/// </summary>
 	void Update () {
+        strDisplay = "";
         Behave();
 	}
 
     void OnGUI()
     {
-        string strDisplay = "";
+        
         if (beacons != null) strDisplay += "beacons : " + beacons.Length;
         if (closestBeacon != null) strDisplay += " closestBeacon : " + closestBeacon.transform.position.ToString();
 
-        //GUI.Label(new Rect(0, 0, Screen.width, Screen.height), strDisplay);
+        GUI.Label(new Rect(0, 0, Screen.width, Screen.height), strDisplay);
     
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void StartWave()
+    {
+        fltHealth = 100.0f;
+        Behavior = enAlienBehaviors.Attack;
     }
 
     /// <summary>
@@ -55,10 +86,12 @@ public class Alien : MonoBehaviour {
     /// </summary>
     private void Behave()
     {
+        GetDamageFromLight();
         if (Behavior == enAlienBehaviors.Attack)
         {
             FindClosestBeacon();
             MoveTowardBeacon();
+            CheckHealth();
         }
         else if (Behavior == enAlienBehaviors.RunAway)
         {
@@ -101,10 +134,30 @@ public class Alien : MonoBehaviour {
     /// </summary>
     private void MoveTowardBeacon()
     {
-        if (closestBeacon != null && (transform.position - closestBeacon.transform.position).magnitude > fltBeaconMinAttackDistance)
+        if (closestBeacon != null )
         {
-            transform.position = Vector3.Lerp(transform.position, closestBeacon.transform.position, Time.deltaTime * fltSpeed);
+            if ((transform.position - closestBeacon.transform.position).magnitude > fltBeaconAttackDistance)
+            {
+                //we are not close enough to do damage
+                transform.position = Vector3.Lerp(transform.position, closestBeacon.transform.position, Time.deltaTime * fltSpeed);
+            }
+            else
+            { 
+                //we are close enough to do damage
+                AttackBeaconClosestBeacon();
+            }
         }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void AttackBeaconClosestBeacon()
+    {
+        //get a ref to the beacon script
+        Beacon beaconScript = closestBeacon.GetComponent<Beacon>();
+        //damage the beacon
+        beaconScript.TakeHit(fltBeaconDamageRate);
     }
 
     /// <summary>
@@ -116,10 +169,15 @@ public class Alien : MonoBehaviour {
         Vector3 vctRunDirection = (transform.position - closestBeacon.transform.position);
         //are we a save distance from it?
         if (vctRunDirection.magnitude < fltLightSafeDistance)
-        { 
+        {
             //we are too close.  run away! run away!
             vctRunDirection = vctRunDirection.normalized;
             transform.position += vctRunDirection * Time.deltaTime * fltSpeed;
+        }
+        else
+        { 
+            //we should be safe now. i will just piddle around out here in outter darkness.
+            Behavior = enAlienBehaviors.Wander;
         }
         
     }
@@ -133,14 +191,33 @@ public class Alien : MonoBehaviour {
         float fltThisDistance = 0.0f;
         float fltIntensity = 0.0f;
         float fltDamage = 0.0f;
-        foreach (GameObject thisBeacon in GameDirector.instance.beacons)
+        if (beacons != null)
         {
-            fltThisDistance = (thisBeacon.transform.position - gameObject.transform.position).magnitude;
-            fltIntensity = GetLightIntensity(thisBeacon);
-            fltDamage = fltIntensity / fltThisDistance;
-            fltHealth -= fltDamage * Time.deltaTime;
+            foreach (GameObject thisBeacon in beacons)
+            {
+                fltThisDistance = (thisBeacon.transform.position - gameObject.transform.position).magnitude;
+                fltIntensity = GetLightIntensity(thisBeacon);
+                fltDamage = fltIntensity / Mathf.Pow(fltThisDistance, 2.0f);
+                if (fltDamage < 0.1f) fltDamage = 0.0f;
+                fltHealth -= fltDamage * Time.deltaTime;
+            }
         }
 
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void CheckHealth()
+    {
+        //Debug.Log(string.Format("health {0:000.000}  thresh {1:000.000} ", fltHealth, fltHealthRunAwayThreshold));
+        //check to see if i am well enough to keep attacking
+        if (fltHealth < fltHealthRunAwayThreshold)
+        {
+            
+            //run away!  run away!
+            Behavior = enAlienBehaviors.RunAway;
+        }
     }
 
     /// <summary>
@@ -150,7 +227,12 @@ public class Alien : MonoBehaviour {
     /// <returns></returns>
     private float GetLightIntensity(GameObject beacon)
     {
-        return 0.5f;
+        //get a ref to the beacon script
+        Beacon beaconScript = beacon.GetComponent<Beacon>();
+        Light light = beaconScript.light;
+        LightSource ls = light.GetComponent<LightSource>();
+        float fltIntensity = ls.intensity;
+        return fltIntensity;
     }
 
 }
--- Assets/Plugins/Beacon.cs
+++ Assets/Plugins/Beacon.cs
@@ -4,27 +4,23 @@ using System.Collections;
 public class Beacon : MonoBehaviour, ITargetable {
 
     public Light light;
-	private float health;
+    public float Health {get; set;}
 
 	private bool lightOn;
 
 	// Use this for initialization
 	void Start () {
-	
+        InitBeacon();
 	}
+    public void InitBeacon()
+    {
+        Health = 100f;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
-
-	public float Health {
-		get {
-			throw new System.NotImplementedException ();
-		}
-		set {
-			throw new System.NotImplementedException ();
-		}
+        float fltIntsty = Health / 100.0f;
+        light.intensity = fltIntsty;
 	}
 
 	public bool LightOn {
@@ -36,8 +32,12 @@ public class Beacon : MonoBehaviour, ITargetable {
 		}
 	}
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="damage"></param>
 	public void TakeHit (float damage)
 	{
-		throw new System.NotImplementedException ();
+		this.Health -= damage;
 	}
 }
