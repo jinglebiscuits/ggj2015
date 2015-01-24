7afb04d86e458844f745a7f90955f6743037b2e4
brascome <brascome@uab.edu>
2015-01-24 15:42:49 -0500

    update to alien ai

--- Assets/Plugins/Alien.cs
+++ Assets/Plugins/Alien.cs
@@ -13,6 +13,10 @@ public class Alien : MonoBehaviour {
 
     private Neil neil = null;
     private Ship ship = null;
+    private float fltLightSafeDistance = 5.0f;
+    private float fltHealth = 100.0f;
+    private float fltSunburn = 0.0f;
+
 
     private GameObject closestBeacon = null;
 
@@ -20,6 +24,7 @@ public class Alien : MonoBehaviour {
 	/// 
 	/// </summary>
 	void Start () {
+        Behavior = enAlienBehaviors.RunAway;
         neil = GameDirector.instance.neil;
         ship = GameDirector.instance.ship;
 	}
@@ -81,6 +86,31 @@ public class Alien : MonoBehaviour {
     private void RunAwayFromBeacon()
     { 
     
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void GetDamageFromLight()
+    { 
+        //iterate through each light source
+        float fltThisDistance = 0.0f;
+        foreach (GameObject thisBeacon in GameDirector.instance.beacons)
+        {
+            fltThisDistance = (thisBeacon.transform.position - gameObject.transform.position).magnitude;
+
+        }
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="beacon"></param>
+    /// <returns></returns>
+    private float GetLightIntensity(GameObject beacon)
+    {
+        return 0.5f;
     }
 
 }
