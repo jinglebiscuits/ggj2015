9cd4f7c55bfa275882dec6b03378168d7ea68526
brascome <brascome@uab.edu>
2015-01-25 12:50:53 -0500

    added win condition and cut scene

---
+++ Assets/Plugins/VideoDirector.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoDirector : MonoBehaviour {
+
+    public MovieTexture movieTexture = null;
+
+	// Use this for initialization
+	void Start () {
+        movieTexture.Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
